6eeea1ceff7bde5be774ad76dbe89ae5d206f88d
pGamelax <pedrogamela2003@gmail.com>
2026-03-05 22:56:39 -0300

    fix client header

--- packages/frontend/src/app/(protected)/layout.tsx
+++ packages/frontend/src/app/(protected)/layout.tsx
@@ -24,7 +24,7 @@ export default async function ProtectedLayout({
 
   return (
     <div className="min-h-screen bg-background">
-      <ClientHeader user={session.user} />
+      <ClientHeader initialUser={session.user} />
       <main>{children}</main>
     </div>
   );
