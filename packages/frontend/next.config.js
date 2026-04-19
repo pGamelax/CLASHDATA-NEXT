29ffb3ce8cee4907f412b53d299e6f0c05efde8f
pGamelax <pedrogamela2003@gmail.com>
2026-04-18 23:54:08 -0300

    adasd

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://v2.clashdata.pro/:path*',
+        permanent: false,
+      },
+    ];
+  },
   output: 'standalone',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
